61ad9bc142cd97d67b9dce65c86465b88dcd88db
Annu Bhatt <bhatt.annu96083@gmail.com>
2022-08-27 22:58:48 +0530

    some changes made

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,11 @@
-def factorial(int x):
+def factorial(x):
+    if ( x ==1 or  x==0):
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
     
 
 x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+print(factorial(x))
