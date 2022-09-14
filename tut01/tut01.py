aa98558a1fc3e32f5a667a1a241a646abc8ed6cd
Annu Bhatt <bhatt.annu96083@gmail.com>
2022-09-14 23:25:49 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -70,3 +70,25 @@ def octact_identification(mod=5000):
 
             nr=ceil(n/mod)
 
+            # overall octant frequency (using a dict)
+            fo={1:0, -1:0, 2:0, -2:0, 3:0, -3:0, 4:0, -4:0}
+
+            # range wise octant frequency
+            f={}
+
+            for i in range (0,nr):
+                f[i]={1:0, -1:0, 2:0, -2:0, 3:0, -3:0, 4:0, -4:0}
+
+            for i in range (0,n):
+                r=int(i/mod)
+                o=get_octant(U_[i], V_[i], W_[i])
+                f[r][o]+=1
+                fo[o]+=1
+
+            with open('octant_output.csv', 'w', newline='') as fileoutput:
+
+                # The names of field
+                fn=['Time', 'U','V', 'W' ,'U Avg','V Avg','W Avg',"U'=U - U avg","V'=V - V avg","W'=W - W avg", "Octant"," ","Octant ID", "+1","-1","+2","-2","+3","-3","+4","-4"]
+
+                writer=csv.writer(fileoutput)
+
