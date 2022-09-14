019bc961e1d35c06711992415100538d7b52d29d
Annu Bhatt <bhatt.annu96083@gmail.com>
2022-09-14 23:12:44 +0530

    step2

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,2 +1,18 @@
 import csv
 from math import ceil, floor
+
+def get_octant(x, y, z):
+    if(x>=0 and y>=0 and z>=0):
+        return("quadrant=1")
+    elif(x>=0 and y>=0 and z<0):
+        return("quadrant=-1")
+    elif(x<0 and y>=0 and z>=0):
+        return("quadrant=2")
+    elif(x<0 and y>=0 and z<0):
+        return("quadrant=-2")
+    elif(x<0 and y<0 and z>=0):
+        return("quadrant=3")
+    elif(x<0 and y<0 and z<0):
+        return("quadrant=-3")
+    elif(x>=0 and y<0 and z>=0):
+        return("quadrant=4")
