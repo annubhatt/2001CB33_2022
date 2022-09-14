c1fc68510ceadf79bec9c3d8ee150e3138add897
Annu Bhatt <bhatt.annu96083@gmail.com>
2022-09-14 22:51:30 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -3,6 +3,10 @@ def factorial(x):
         return 1
     else:
         return (x * factorial(x-1))
+<<<<<<< Updated upstream
 
+=======
+        
+>>>>>>> Stashed changes
 x=int(input("Enter the number whose factorial is to be found"))
 factorial(x)
