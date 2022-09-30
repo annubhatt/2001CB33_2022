1eba5b1b7015da2d8489db729a4d132dd11aeb4c
Annu Bhatt <bhatt.annu96083@gmail.com>
2022-09-30 23:33:54 +0530

    Update tut02.py
    ggh

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -1,13 +1,53 @@
-def octant_transition_count(mod=5000):
-###Code
 
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
+# here we define mod value
 mod=5000
-octant_transition_count(mod)
+
+# importing openpyxl and nan and loading workbook
+try:
+    from cmath import nan
+    import openpyxl
+
+    wb = openpyxl.load_workbook(r'C:\Users\DELL\OneDrive\Desktop\temp_octant\input_octant_transition_identify - Copy.xlsx')
+except:
+    print("there is error in loading workbook check your file directory and import openpyxl")
+    exit()
+
+
+    # calculation average value
+try:
+    sheet = wb.active
+    Uavg=0
+    Vavg=0
+    Wavg=0
+    lst=[]
+    for row in sheet.iter_rows(row_minm=1, col_minm=1,row_maxm=29746, col_maxm=4):
+        lst1=[]
+        for cell in row:
+            lst1.append(cell.value)
+        lst.append(lst1)
+    for i in range(1,29746):
+        Uavg=Uavg+lst[i][1]
+        Vavg=Vavg+lst[i][2]
+        Wavg=Wavg+lst[i][3]
+    Uavg=(Uavg/29745)
+    Vavg=(Vavg/29745)
+    Wavg=(Wavg/29745)
+
+    lst_avg=[["Uavg","Vavg","Wavg"],[Uavg,Vavg,Wavg]]
+except:
+    print("there is error in calculating average value")
+    exit()
+
+#  nan in tansition value is written
+
+try:
+    i=0
+    for row in sheet.iter_rows(row_minm=1, col_minm=12,row_maxm=29777, col_maxm=21):
+        j=0
+        for cell in row:
+            cell.value=nan
+            j=j+1
+        i=i+1
+except:
+    print("there is some error in transition count")
+#create octant of provided value
