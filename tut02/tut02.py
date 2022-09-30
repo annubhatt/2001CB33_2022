98ff81c228dba61801aaa55c55ff23aa6d94622b
Annu Bhatt <bhatt.annu96083@gmail.com>
2022-09-30 23:35:34 +0530

    Update tut02.py

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -51,3 +51,63 @@ try:
 except:
     print("there is some error in transition count")
 #create octant of provided value
+
+try:
+    i=0
+    for row in sheet.iter_rows(row_minm=1, col_minm=5,row_maxm=2, col_maxm=7):
+        j=0
+        for cell in row:
+            cell.value=lst_avg[i][j]
+            j=j+1
+        i=i+1
+    lst_newval=[]
+    for i in range(1,29746):
+        lst_newval.append([lst[i][1]-Uavg,lst[i][2]-Vavg,lst[i][3]-Wavg])
+    sheet.cell(row=1,column=8).value="Uavg'"
+    sheet.cell(row=1,column=9).value="Vavg'"
+    sheet.cell(row=1,column=10).value="Wavg'"    
+    sheet.cell(row=1,column=11).value="Octant"    
+
+    i=0
+    for row in sheet.iter_rows(row_minm=2, col_minm=8,row_maxm=29746, col_maxm=10):
+        j=0
+        for cell in row:
+            cell.value=lst_newval[i][j]
+            j=j+1
+        i=i+1
+
+    lst_octant = []
+    for p in lst_newval:
+        if(p[0]>=0):
+            if(p[1]>=0):
+                if(p[2]>=0):
+                    lst_octant.append(1)
+                else:
+                    lst_octant.append(-1)
+            else:
+                if(p[2]>=0):
+                    lst_octant.append(4)
+                else:
+                    lst_octant.append(-4)
+        else:
+            if(p[1]>=0):
+                if(p[2]>=0):
+                    lst_octant.append(2)
+                else:
+                    lst_octant.append(-2)
+            else:
+                if(p[2]>=0):
+                    lst_octant.append(3)
+                else:
+                    lst_octant.append(-3)
+
+    i=0
+    for row in sheet.iter_rows(row_minm=2, col_minm=11,row_maxm=29746, col_maxm=11):
+        for cell in row:
+            cell.value=lst_octant[i]
+        i=i+1
+except:
+    print("there is error in creating octant value")
+    exit()
+
+# some error in creating overall count
