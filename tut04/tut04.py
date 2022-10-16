f86df1551cace684cf63afa988ac241945e70b29
Annu Bhatt <bhatt.annu96083@gmail.com>
2022-10-16 23:51:26 +0530

    Update tut04.py

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,14 +1,9 @@
-#Help https://youtu.be/H37f_x4wAC0
-def octant_longest_subsequence_count_with_range():
-###Code
+import openpyxl
+from datetime import datetime
 
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-
-octant_longest_subsequence_count_with_range()
+workbook = openpyxl.load_workbook(filename='output_octant_longest_subsequence_with_range.xlsx')
+sheet = workbook.active
+    # The title values are modified accordingly
+sheet['M1'], sheet['P1'], sheet['Q1'], sheet['R1'], sheet['S1'] = 'Octant', '', 'Octant', 'Longest Subsequence Length', 'Count'
+    # We save the workbook
+workbook.save(filename='output_octant_longest_subsequence_with_range.xlsx')
